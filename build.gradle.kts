697ea8c3b48cd7370bc8a9d15ab96610602da1d3
KhaiPV <khaipv@piontech.co>
2025-05-08 17:15:43 +0700

    build(gradle): remove safe arg navigation

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
-    alias(libs.plugins.androidx.navigation.safeargs.kotlin) apply false
     alias(libs.plugins.dagger.hilt.android) apply false
     alias(libs.plugins.gms.google.services) apply false
     alias(libs.plugins.google.firebase.crashlytics) apply false
